5011604265215957a3db13c937585f3b0b83443d
Daniel McKeown <2014@pacificpelican.us>
2017-05-29 21:39:43 -0700

    add yogachat markup to top of view

--- src/App.js
+++ src/App.js
@@ -23,12 +23,60 @@ class ChatApp extends Component {
   }
 }
 
+class MyInput extends Component {
+	constructor(props) {
+  	super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  
+  handleChange(e) {
+  	console.log('e', e.target.vaule);
+  }
+	
+	handleClick() {
+		var elements = document.querySelectorAll('input');
+		let chatContent = elements[0].value;
+		console.log(chatContent);
+		let el = "<div className='chat_item'>" + chatContent + "</div>";
+		var p = document.createElement("p");
+		var newDiv = document.createElement("div"); 
+		var newContent = document.createTextNode(chatContent); 
+  	newDiv.appendChild(newContent); //add the text node to the newly created div. 
+		var xy = document.getElementById('chats');
+		var aChild = xy.appendChild(newDiv);
+		var zx = document.getElementById('chat_input');
+		var clearInput = zx.value = '';
+	}	
+	
+  render() {
+  	return (
+		<div id="innerContainer">
+			<div id="title_area">
+				<h1 class="nasty_chat">
+					YOGA CHAT
+				</h1>
+				<h5 id="tagline">
+					from <a href="http://danmckeown.info">Dan McKeown</a>
+				</h5>
+			</div>
+			<main className="chat_box">
+			<chatContent /><ChatApp />
+				<div className="user_input" id="root0">
+					<input id="chat_input" className="user_chat" />
+					<button onClick={this.handleClick} className="user_button">
+						send
+					</button>
+				</div>
+			</main></div>
+		)
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-                <ChatApp />
+                <MyInput />
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
--- src/ychat.js
+++ src/ychat.js
@@ -24,12 +24,12 @@ class ChatApp extends Component {
   
 
 class MyInput extends React.Component{
-	constructor(props){
+	constructor(props) {
   	super(props);
     this.handleChange = this.handleChange.bind(this);
   }
   
-  handleChange(e){
+  handleChange(e) {
   	console.log('e', e.target.vaule);
   }
 	
@@ -39,38 +39,36 @@ class MyInput extends React.Component{
 		console.log(chatContent);
 		let el = "<div className='chat_item'>" + chatContent + "</div>";
 		var p = document.createElement("p");
-		
 		var newDiv = document.createElement("div"); 
 		var newContent = document.createTextNode(chatContent); 
   	newDiv.appendChild(newContent); //add the text node to the newly created div. 
-
 		var xy = document.getElementById('chats');
-  
 		var aChild = xy.appendChild(newDiv);
 		var zx = document.getElementById('chat_input');
 		var clearInput = zx.value = '';
 	}	
 	
-  render(){
-  	return <div id="innerContainer">
-<div id="title_area">
-	<h1 class="nasty_chat">
-		YOGA CHAT
-	</h1>
-	<h5 id="tagline">
-		from <a href="http://danmckeown.info">Dan McKeown</a>
-	</h5>
-</div>
-		
-<main className="chat_box">
-<chatContent /><DataDisplay0 />
-	<div className="user_input" id="root0">
-		<input id="chat_input" className="user_chat" />
-		<button onClick={this.handleClick} className="user_button">
-			send
-		</button>
-	</div>
-</main></div>
+  render() {
+  	return (
+		<div id="innerContainer">
+			<div id="title_area">
+				<h1 class="nasty_chat">
+					YOGA CHAT
+				</h1>
+				<h5 id="tagline">
+					from <a href="http://danmckeown.info">Dan McKeown</a>
+				</h5>
+			</div>
+			<main className="chat_box">
+			<chatContent /><DataDisplay0 />
+				<div className="user_input" id="root0">
+					<input id="chat_input" className="user_chat" />
+					<button onClick={this.handleClick} className="user_button">
+						send
+					</button>
+				</div>
+			</main></div>
+		)
   }
 }
 //	ReactDOM.render(<MyInput/>, document.getElementById('myyogachat'));
