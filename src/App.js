b5d3e8df4a8584c316d28723567e0f742f0fcbd1
Daniel McKeown <2014@pacificpelican.us>
2017-05-29 21:44:37 -0700

    remove create-react-app boilerplate CSS and HTML

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ class MyInput extends Component {
 					</button>
 				</div>
 			</main></div>
-		)
+		);
   }
 }
 
@@ -76,14 +76,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-                <MyInput />
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <MyInput />
       </div>
     );
   }
