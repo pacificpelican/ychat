30e498ff667072b9f22b9d0a66d9b0e62e000610
Daniel McKeown <2014@pacificpelican.us>
2017-06-05 20:05:07 -0700

    data entered at form at /register is displayed by Express after submission -reg smoke test

--- eapp.js
+++ eapp.js
@@ -2,23 +2,45 @@
 
 const express = require('express');
 const app = express();
+
+var bodyParser = require('body-parser');
+
 var path = require('path');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+//  app.use( bodyParser.json() );       // to support JSON-encoded bodies
 
 var MongoClient = require('mongodb').MongoClient;
 
 MongoClient.connect('mongodb://localhost:27017/testdb', function (err, db) {
-  if (err) console.log(err);
-      //throw err 
-    db.collection('testcollection').insertOne( {
-      "key" :  Math.floor((Math.random() * 9) + 1)  });
-    db.collection('testcollection').find().toArray(function (err, result) {
-      if (err) console.log(err);
+  if (err)  {
+    console.log(err);
+    throw err;
+  }
+  db.collection('testcollection').insertOne( {"username" :  "x", "password": "y"});
+  db.collection('testcollection').find().toArray(function (err, result) {
+    if (err) {
+      console.log(err);
+    }
+    else {
       console.log(result);
-    });
+    }
+  });
 });
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname + '/build/index.html'));
+});
+
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname + '/register.html'));
+});
+
+app.post('/process/register', (req, res) => {
+  var user_name = req.body.login;
+  var user_pw = req.body.password;
+  console.log(req.body);
+  res.send(user_name + ' ' + user_pw);
 });
 
 app.use('/static', express.static('build/static'));
