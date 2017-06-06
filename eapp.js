048e28cb879c4e47e641d9db2a358dbf2a0d61f7
Daniel McKeown <2014@pacificpelican.us>
2017-06-05 19:35:44 -0700

    add arbitrary creates and reads to MongoDB when Express app runs

--- eapp.js
+++ eapp.js
@@ -4,6 +4,19 @@ const express = require('express');
 const app = express();
 var path = require('path');
 
+var MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/testdb', function (err, db) {
+  if (err) console.log(err);
+      //throw err 
+    db.collection('testcollection').insertOne( {
+      "key" :  Math.floor((Math.random() * 9) + 1)  });
+    db.collection('testcollection').find().toArray(function (err, result) {
+      if (err) console.log(err);
+      console.log(result);
+    });
+});
+
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname + '/build/index.html'));
 });
