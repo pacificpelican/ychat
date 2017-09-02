90326d80bb73037cca3020578e89cedd372ebde1
Daniel McKeown <2014@pacificpelican.us>
2017-09-01 21:36:31 -0700

    v1.0.0 format JS with Prettier, update npm scripts, new favicon 🕊️

--- eapp.js
+++ eapp.js
@@ -1,202 +1,155 @@
 //  ExpressJS startup page for ychat App [LokiJS version] by Dan McKeown http://danmckeown.info
 //	ychat.pacificio.com | copyright 2017
 
-var express = require('express');
+var express = require("express");
 var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
 
-const crypto = require('crypto');
+const crypto = require("crypto");
 
-const secret = 'abcdefg';
+const secret = "abcdefg";
 
 function getHash(src) {
-  const hash = crypto.createHmac('sha256', src)
-  .update('I love cupcakes')  // via https://nodejs.org/api/crypto.html
-  .digest('hex');
+  const hash = crypto
+    .createHmac("sha256", src)
+    .update("I love cupcakes") // via https://nodejs.org/api/crypto.html
+    .digest("hex");
   console.log(src + " hashed is " + hash);
   return hash;
 }
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-    console.log('message: ' + msg);
+io.on("connection", function(socket) {
+  socket.on("chat message", function(msg) {
+    io.emit("chat message", msg);
+    console.log("message: " + msg);
   });
 });
 
-server.listen(process.env.PORT || 3000, function () {
-  console.log('ychat app listening on port 3000');
+server.listen(process.env.PORT || 3000, function() {
+  console.log("ychat app listening on port 3000");
 });
 
-var bodyParser = require('body-parser');
-var path = require('path');
+var bodyParser = require("body-parser");
+var path = require("path");
 
-var Strategy = require('passport-http').BasicStrategy;
-var loki = require('lokijs')
-var db = new loki(__dirname + '/users.json')
+var Strategy = require("passport-http").BasicStrategy;
+var loki = require("lokijs");
+var db = new loki(__dirname + "/users.json");
 
-var passport = require('passport');
+var passport = require("passport");
 
-io.on('connection', function(socket){
-  console.log('a user connected [socketIO]');
+io.on("connection", function(socket) {
+  console.log("a user connected [socketIO]");
 });
 
-let userfiles = 'userfiles';
+let userfiles = "userfiles";
 
-db.loadDatabase({}, function () {
+db.loadDatabase({}, function() {
   let _collection = db.getCollection(userfiles);
 
   if (!_collection) {
     console.log("Collection %s does not exit. Creating ...", userfiles);
-      _collection = db.addCollection(userfiles);
+    _collection = db.addCollection(userfiles);
   }
 
   var userList = _collection.where(function(obj) {
-    return (obj.sentTime != null);
+    return obj.sentTime != null;
   });
- // console.log("user list: " +  userList);
-  //  var userList = _collection.find({ vYear:'2017' });
 });
 
-// MongoClient.connect('mongodb://localhost:27017/testdb', function (err, db) {
-//   if (err)  {
-//     console.log(err);
-//     throw err;
-//   }
-//   db.collection('testcollection').find().toArray(function (err, result) {
-//     if (err) {
-//       console.log(err);
-//     }
-//   });
-// });
-
 // Configure the Basic strategy for use by Passport.
-//
-// The Basic strategy requires a `verify` function which receives the
-// credentials (`username` and `password`) contained in the request.  The
-// function must verify that the password is correct and then invoke `cb` with
-// a user object, which will be set at `req.user` in route handlers after
-// authentication.
-passport.use(new Strategy(
-  function(username, password, cb) {
-//    db.users.findByUsername(username, function(err, user) {
-//  console.log('submitted username and userpassword: ' + username + " " + password);
-  db.loadDatabase({}, function () {
-    let _collection = db.getCollection(userfiles);
-  
-    if (!_collection) {
-      console.log("Collection %s does not exit. Creating ...", userfiles);
-        _collection = db.addCollection(userfiles);
-    }
+passport.use(
+  new Strategy(function(username, password, cb) {
+    db.loadDatabase({}, function() {
+      let _collection = db.getCollection(userfiles);
 
- //   var userList = _collection.find( { "username": { $eq: username } } ).toArray(function (err, result) {
-    var userList = _collection.find( { "username": { $eq: username } } );
+      if (!_collection) {
+        console.log("Collection %s does not exit. Creating ...", userfiles);
+        _collection = db.addCollection(userfiles);
+      }
+
+      var userList = _collection.find({ username: { $eq: username } });
       console.log("userList:");
-        console.log(userList);
-        var hashedPW = userList[0].userpassword;
-        if (userList.length > 0) {
-          if (hashedPW == getHash(password)) {
-            return cb(null, username);
-          }
-          else {
+      console.log(userList);
+      var hashedPW = userList[0].userpassword;
+      if (userList.length > 0) {
+        if (hashedPW == getHash(password)) {
+          return cb(null, username);
+        } else {
           return cb(null, false);
-          }
         }
-        else {
-          return cb(err);
-        }
+      } else {
+        return cb(err);
+      }
+    });
   })
-  // MongoClient.connect('mongodb://localhost:27017/testdb', function (err, db) {
-  //   if (err)  {
-  //     console.log(err);
-  //     throw err;
-  //   }
-  //     db.collection('testcollection').find( { "username": { $eq: username } } ).toArray(function (err, result) {
-  //       console.log("result:")
-  //         console.log(result)
-  //         var hashedPW = result[0].userpassword;
-  //         if (result.length > 0) {
-  //           if (hashedPW == getHash(password)) {
-  //             return cb(null, username);
-  //           }
-  //           else {
-  //           return cb(null, false);
-  //           }
-  //         }
-  //         else {
-  //           return cb(err);
-  //         }
-  //       })
-  //     });
-    }));
+);
 
 var _user_name = null;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-//  app.use( bodyParser.json() );       // to support JSON-encoded bodies
 
-app.get('/', passport.authenticate('basic', { session: false }), (req, res) => {
+app.get("/", passport.authenticate("basic", { session: false }), (req, res) => {
   console.log("req.user");
   console.log(req.user);
   _user_name = req.user;
-  res.sendFile(path.resolve(__dirname + '/build/index.html'));
+  res.sendFile(path.resolve(__dirname + "/build/index.html"));
 });
 
-app.get('/currentusername', passport.authenticate('basic', { session: false }), (req, res) => {
-  res.send(_user_name);
+app.get(
+  "/currentusername",
+  passport.authenticate("basic", { session: false }),
+  (req, res) => {
+    res.send(_user_name);
+  }
+);
+
+app.get("/build", express.static("build"));
+
+app.use("/socket.io", express.static("node_modules/socket.io/socket.js"));
+
+app.get(
+  "/info/:username",
+  passport.authenticate("basic", { session: false }),
+  function(req, res) {
+    res.send(req.params);
+  }
+);
+
+app.get("/service-worker.js", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "/build/service-worker.js"));
 });
 
-app.get('/build', express.static('build'));
-
-app.use('/socket.io', express.static('node_modules/socket.io/socket.js'));
-
-app.get('/info/:username', passport.authenticate('basic', { session: false }), function (req, res) {
-  res.send(req.params)
-})
-
-app.get('/service-worker.js', (req, res) => {
-  res.sendFile(path.resolve(__dirname + '/build/service-worker.js'));
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname + "/register.html"));
 });
 
-app.get('/register', (req, res) => {
-  res.sendFile(path.join(__dirname + '/register.html'));
-});
-
-app.post('/process/register', (req, res) => {
+app.post("/process/register", (req, res) => {
   var user_name = req.body.login;
   var user_pw = getHash(req.body.password);
   console.log(req.body);
   let homeLink = "<a href='../../..'>Home</a>";
   let createdAt = Date.now();
 
-db.loadDatabase({}, function () {
-  let _collection = db.getCollection(userfiles);
+  db.loadDatabase({}, function() {
+    let _collection = db.getCollection(userfiles);
 
-  if (!_collection) {
+    if (!_collection) {
       console.log("Collection %s does not exit. Creating ...", userfiles);
       _collection = db.addCollection(userfiles);
-    
-  }
+    }
 
-  _collection.insertOne( {"username" :  user_name, "userpassword": user_pw, "created_at": createdAt});
-  //  _collection.insert(actor1);
-      
-  db.saveDatabase();
+    _collection.insertOne({
+      username: user_name,
+      userpassword: user_pw,
+      created_at: createdAt
+    });
 
-  res.send(user_name + ' ' + user_pw + ' created    | ' + homeLink);
+    db.saveDatabase();
+
+    res.send(user_name + " " + user_pw + " created    | " + homeLink);
+  });
 });
 
-  // MongoClient.connect('mongodb://localhost:27017/testdb', function (err, db) {
-  //   if (err)  {
-  //     console.log(err);
-  //     throw err;
-  //   }
-  //   else {
-  //     db.collection('testcollection').insertOne( {"username" :  user_name, "userpassword": user_pw, "created_at": createdAt});
-  //   }
-  //   res.send(user_name + ' ' + user_pw + ' created    | ' + homeLink);
-  // });
-});
-
-app.use('/static', express.static('build/static'));
+app.use("/static", express.static("build/static"));
--- src/App.js
+++ src/App.js
@@ -1,155 +1,181 @@
 //	ychat App by Dan McKeown http://danmckeown.info
 //	ychat.pacificio.com | copyright 2017
 
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import io from 'socket.io-client';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import io from "socket.io-client";
 
-var socket = require('socket.io-client')('http://localhost:3000');
+var socket = require("socket.io-client")("http://localhost:3000");
 
-var request = require('superagent');
+var request = require("superagent");
 
 console.log(socket);
 
-	socket.on('chat message', function(msg) {
-    console.log('message received');
-		console.log(msg);
-  });
+socket.on("chat message", function(msg) {
+  console.log("message received");
+  console.log(msg);
+});
 
 function getParameterByName(name, url) {
-	if (!url) url = window.location.href;
-	name = name.replace(/[\[\]]/g, "\\$&");
-	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-		results = regex.exec(url);
-	if (!results) return null;
-	if (!results[2]) return '';
-	return decodeURIComponent(results[2].replace(/\+/g, " "));
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return "";
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
-var params = getParameterByName('username');
+var params = getParameterByName("username");
 
 class ChatApp extends Component {
-		render() {
-    return(
-    <section id="chats" className="chat_data">
-			
-		</section>
-    );
+  render() {
+    return <section id="chats" className="chat_data" />;
   }
 }
 
 class MyInput extends Component {
-	constructor(props) {
-  	super(props);
+  constructor(props) {
+    super(props);
 
-		socket.on('chat message', function(msg) {
-          console.log("receiving chat message: " + msg.msg + " " + msg.sender + " " + msg.sentTime);
-					var newDiv = document.createElement("div");
-					let processedTime = ((((msg.sentTime / 60) / 60) / 24 / 1000 / 365) + 1970);
-					let chatContent = msg.sender + ": " + msg.msg;
-					let chatMeta = "sent " + processedTime + " ";
-					var newContent = document.createTextNode(chatContent); 
-					newDiv.appendChild(newContent); //add the text node to the newly created div.
-					var xy = document.getElementById('chats');
-					var aChild = xy.appendChild(newDiv);
-					var newSec = document.createElement("aside");
-					var newMetaContent = document.createTextNode(chatMeta); 
-					newSec.appendChild(newMetaContent); //add the text node to the newly created div.
-					var xy2 = document.getElementById('chats');
-					var aChild2 = xy.appendChild(newSec);
-        });
+    socket.on("chat message", function(msg) {
+      console.log(
+        "receiving chat message: " +
+          msg.msg +
+          " " +
+          msg.sender +
+          " " +
+          msg.sentTime
+      );
+      var newDiv = document.createElement("div");
+      let processedTime = msg.sentTime / 60 / 60 / 24 / 1000 / 365 + 1970;
+      let chatContent = msg.sender + ": " + msg.msg;
+      let chatMeta = "sent " + processedTime + " ";
+      var newContent = document.createTextNode(chatContent);
+      newDiv.appendChild(newContent); //add the text node to the newly created div.
+      var xy = document.getElementById("chats");
+      var aChild = xy.appendChild(newDiv);
+      var newSec = document.createElement("aside");
+      var newMetaContent = document.createTextNode(chatMeta);
+      newSec.appendChild(newMetaContent); //add the text node to the newly created div.
+      var xy2 = document.getElementById("chats");
+      var aChild2 = xy.appendChild(newSec);
+    });
 
-		console.log("params: " + params);
+    console.log("params: " + params);
     this.handleChange = this.handleChange.bind(this);
   }
-  
+
   handleChange(e) {
-  	console.log('e', e.target.vaule);
+    console.log("e", e.target.vaule);
   }
-	
-	handleClick() {
-		let elements1 = document.getElementById("chat_input");
-		let chatContent = elements1.value;
-		console.log(chatContent);
 
-		var userNameSpanCurrentID  = document.getElementById('showusername');
-		var userNameText = userNameSpanCurrentID.textContent;
+  handleClick() {
+    let elements1 = document.getElementById("chat_input");
+    let chatContent = elements1.value;
+    console.log(chatContent);
 
-		let setToNothing = document.getElementById("chat_input").value = "";
-    socket.emit('chat message', {msg: chatContent, sender: userNameText, sentTime: Date.now()});
-	}
+    var userNameSpanCurrentID = document.getElementById("showusername");
+    var userNameText = userNameSpanCurrentID.textContent;
 
-	handleKeyPress = (event) => {
-		if(event.key == 'Enter'){
-			this.handleClick();
-		}
-	}
-
-	componentDidMount() {   
-		console.log('MyInput component did mount');
-		var userNameSpan  = document.getElementById('showusername');
-		request.
-		get('/currentusername')
-		.end(function(err, res){
-			var putInUserNameLater = userNameSpan.textContent = res.text;
-			console.log('userNameSpan text updated to ' + res.text);
-		});
+    let setToNothing = (document.getElementById("chat_input").value = "");
+    socket.emit("chat message", {
+      msg: chatContent,
+      sender: userNameText,
+      sentTime: Date.now()
+    });
   }
-	
+
+  handleKeyPress = event => {
+    if (event.key == "Enter") {
+      this.handleClick();
+    }
+  };
+
+  componentDidMount() {
+    console.log("MyInput component did mount");
+    var userNameSpan = document.getElementById("showusername");
+    request.get("/currentusername").end(function(err, res) {
+      var putInUserNameLater = (userNameSpan.textContent = res.text);
+      console.log("userNameSpan text updated to " + res.text);
+    });
+  }
+
   render() {
-  	return (
-		<div id="innerContainer">
-			<div id="title_area">
-				<h1 className="nasty_chat">
-					Y CHAT
-				</h1>
-				<h5 id="tagline">
-					from <a href="http://danmckeown.info">Dan McKeown</a>
-				</h5>
-				<h6 id="usernameheader">
-					<span id="showusernamelabel" className="userinfo">user:</span> <span id="showusername" className="userinfo"></span>
-				</h6>
-				<p>
-					<span id="cantclick"><a href="#" onClick={function() { alert('ok the cursor should be free')}}>just type!</a></span>
-				</p>
-				<p>
-					<span id="ychatlink"><a href="http://ychat.pacificio.com">ychat on pacificio</a></span>
-				</p>
-			</div>
-			<main className="chat_box">
-			<chatContent /><ChatApp />
-				<div className="user_input" id="root0">
-					<span><input type="text" id="chat_input" className="user_chat" onKeyUp={this.handleKeyPress} /></span>
-					<button onClick={this.handleClick} className="user_button">
-						send
-					</button>
-				</div>
-			</main></div>
-		);
+    return (
+      <div id="innerContainer">
+        <div id="title_area">
+          <h1 className="nasty_chat">Y CHAT</h1>
+          <h5 id="tagline">
+            from <a href="http://danmckeown.info">Dan McKeown</a>
+          </h5>
+          <h6 id="usernameheader">
+            <span id="showusernamelabel" className="userinfo">
+              user:
+            </span>{" "}
+            <span id="showusername" className="userinfo" />
+          </h6>
+          <p>
+            <span id="cantclick">
+              <a
+                href="#"
+                onClick={function() {
+                  alert("ok the cursor should be free");
+                }}
+              >
+                just type!
+              </a>
+            </span>
+          </p>
+          <p>
+            <span id="ychatlink">
+              <a href="http://ychat.pacificio.com">ychat on pacificio</a>
+            </span>
+          </p>
+        </div>
+        <main className="chat_box">
+          <chatContent />
+          <ChatApp />
+          <div className="user_input" id="root0">
+            <span>
+              <input
+                type="text"
+                id="chat_input"
+                className="user_chat"
+                onKeyUp={this.handleKeyPress}
+              />
+            </span>
+            <button onClick={this.handleClick} className="user_button">
+              send
+            </button>
+          </div>
+        </main>
+      </div>
+    );
   }
 }
 
 class App extends Component {
-	constructor() {
-		super();
-	}
+  constructor() {
+    super();
+  }
 
-  componentDidMount() {   
-		console.log(socket);
-		document.getElementById("chat_input").focus();
+  componentDidMount() {
+    console.log(socket);
+    document.getElementById("chat_input").focus();
   }
 
   sendMessage = message => {
-    socket.emit(`client:sendMessage`, message)
-  }
+    socket.emit(`client:sendMessage`, message);
+  };
 
   render() {
-				return (
-				<div className="App">
-					<MyInput />
-				</div>
-			);
+    return (
+      <div className="App">
+        <MyInput />
+      </div>
+    );
   }
 }
 
